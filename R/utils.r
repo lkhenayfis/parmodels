808b0d3cfd4de4fdd935a0878dbf9f6fa9f7c0e3
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-12-16 15:34:24 -0300

    Add funcoes p padronizacao sazonal

--- R/utils.r
+++ R/utils.r
@@ -28,3 +28,56 @@ cov2 <- function(x, na.rm = FALSE) {
     use <- ifelse(na.rm, "pairwise.complete.obs", "everything")
     (n - 1) / n * cov(x = x, use = use)
 }
+
+split_by_season <- function(serie) {
+    seasons <- c(cycle(serie))
+    orig_order <- order(unlist(split(seq_along(serie), seasons)))
+    splitted   <- split(serie, seasons)
+    attr(splitted, "orig_order") <- orig_order
+    return(splitted)
+}
+
+#' Normaliza Serie Sazonalmente
+#' 
+#' Normaliza uma serie temporal com sazonalidade utilizando medias e desvios padrao por estacao
+#' 
+#' Caso `means` e `sds` sejam fornecidos, devem ser vetores com comprimento igual a a frequencia
+#' sazonal da serie, em que a primeira posicao corresponde à primeira estacao e assim por diante.
+#' 
+#' @param serie serie temporal com sazonalidade para normalizar
+#' @param est um de `c("n", "n-1")` indicando qual denominador utilizar para desvio padrao
+#' @param means,sds medias e desvios padrao sazonais ja calculadas (opcional)
+#' 
+#' @return lista de dois elementos: serie temporal padronizada sazonalmente e lista contendo medias
+#'     e desvios padrao sazonais
+
+scale_by_season <- function(serie, est = "n", means = NULL, sds = NULL) {
+
+    attr0 <- attributes(serie)
+
+    if (is.null(means)) means <- seasonal_mean(serie)
+    if (is.null(sds))   sds   <- seasonal_sd(serie, est)
+
+    serie <- split_by_season(serie)
+    order0 <- attr(serie, "orig_order")
+    serie <- mapply(function(x, m, s) (x - m) / s, serie, means, sds, SIMPLIFY = FALSE)
+    serie <- unname(unlist(serie)[order0])
+
+    attributes(serie) <- attr0
+
+    out <- list(serie, c(means, sds))
+    return(out)
+}
+
+seasonal_mean <- function(serie) {
+    splitted <- split_by_season(serie)
+    means <- sapply(splitted, function(x) mean(x, na.rm = TRUE))
+    return(means)
+}
+
+seasonal_sd <- function(serie, est = "n") {
+    fsd <- ifelse(est == "n-1", sd, sd2)
+    splitted <- split_by_season(serie)
+    sds <- sapply(splitted, function(x) fsd(x, na.rm = TRUE))
+    return(sds)
+}
