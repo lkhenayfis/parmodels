3bac3b5a955c443cb6bcad4b4aa0b3b89acc246f
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-12-17 10:28:15 -0300

    Init funcionalidades de modelo par simples

---
+++ R/classe-parmodel.r
@@ -0,0 +1,123 @@
+#' Estimacao De `par`s
+#' 
+#' Estimacao de modelos autorregressivos periodicos
+#' 
+#' O argumento `ps` pode ser ou um escalar ou um vetor. No caso de ser escalar, sera repetido para N
+#' vezes, onde N é a frequencia sazonal da serie. Caso seja um vetor, deve ter comprimento igual a
+#' frequencia sazonal da serie. Em ambos os casos, o valor em cada posicao indica a ordem do modelo
+#' autorregressivo periodico para a estacao correspondente. Sendo escalar ou vetor, o valor "auto"
+#' pode ser utilizado para indicar que a ordem deve ser identificada automaticamente utilizando o
+#' criterio correspondente a `metodo`: se Yule-Walker, analise das pacfs periodicas.
+#' 
+#' @param serie serie temporal com sazonalidade
+#' @param ps ordens de cada modelo periodico. Veja Detalhes
+#' @param max_ps se nao forem fornecidas ordens, valor maximo a ser considerado para identificacao
+#'     automatica
+#' @param metodo metodo de estimacao. Atualmente, apenas "Yule-Walker" é suportado
+#' 
+#' @return objeto da classe `par`, uma lista contendo os seguintes elementos:
+#'    \itemize{
+#'        \item{phis}{lista contendo os coeficientes de cada modelo autorregressivo periodico}
+#'        \item{sigma2}{variancia dos residuos do modelo}
+#'        \item{serie}{serie temporal utilizada na estimacao}
+#'        \item{call}{chamada da funcao}
+#'   }
+
+par <- function(serie, ps = "auto", max_ps = frequency(serie) - 1, metodo = "YuleWalker") {
+
+    s <- frequency(serie)
+    if (s == 1) stop("'serie' nao possui sazonalidade")
+
+    if (length(ps) < s) ps <- rep(ps, length.out = s)
+    if (length(max_ps) < s) max_ps <- rep(max_ps, length.out = s)
+
+    phis <- lapply(seq_len(s), function(m) fit_par(serie, m, ps[m], metodo, max_p = max_ps[m]))
+
+    cc <- match.call()
+    new <- new_par(x = serie, phis = phis, sigma2 = NA_real_, residuals = NULL, call = cc)
+
+    res <- residuals(new)
+    sigma2 <- var(res, na.rm = TRUE)
+
+    new$residuals <- res
+    new$sigma2 <- sigma2
+
+    return(new)
+}
+
+fit_par <- function(serie, m, p, metodo, ...) {
+    fitfun <- paste0("fit_par_", tolower(metodo))
+    cc <- match.call()
+    cc[[1]] <- as.name(fitfun)
+    fit <- eval(cc, parent.frame(), parent.frame())
+    return(fit)
+}
+
+fit_par_yulewalker <- function(serie, m, p, max_p, ...) {
+    serie <- scale_by_season(serie, est = "n")[[1]]
+    if (p == "auto") p <- idordem_yulewalker(serie, m, max_p)
+    pacf_res <- perpacf(serie, m, p)
+    phis <- pacf_res$phi
+    return(phis)
+}
+
+#' Construtor Interno De `par`
+#' 
+#' Funcao interna para construcao de objetos da classe `par`
+
+new_par <- function(x, phis, sigma2, residuals, call) {
+    new <- list(
+        phis = phis,
+        sigma2 = sigma2,
+        x = x,
+        residuals = residuals,
+        call = call
+    )
+    class(new) <- c("par", "list")
+    return(new)
+}
+
+# METODOS ------------------------------------------------------------------------------------------
+
+fitted.par <- function(object, ...) {
+    if (!is.null(residuals(object))) {
+        res <- residuals(object)
+        fitted <- object$x - res
+    } else {
+        fitted <- filter_series(object)
+    }
+
+    return(fitted)
+}
+
+fitted.values.par <- function(object, ...) fitted.par(object, ...)
+
+residuals.par <- function(object, ...) {
+    if (is.null(object$residuals)) {
+        fitted <- filter_series(object)
+        res <- object$x - fitted
+    } else {
+        res <- object$residuals
+    }
+
+    return(res)
+}
+
+predict.par <- function(object, n.ahead, ...) {
+    predict_series(object, n.ahead)
+}
+
+# AUXILIARES ---------------------------------------------------------------------------------------
+
+#' Identificacao Automatica De Ordem De Modelos Autorregressivos Periodicos
+#' 
+#' Funcao interna para identificar automaticamente a ordem de modelos autorregressivos periodicos
+
+idordem_yulewalker <- function(serie, m, max_p) {
+    serie <- scale_by_season(serie, est = "n")[[1]]
+    phis <- perpacf(serie, m, max_p)
+    conf <- qnorm((1 + .95) / 2) / sqrt(phis$n_used)
+    over <- abs(phis$phi) >= conf
+    ordem <- ifelse(any(over), max(which(over)), 1)
+    return(ordem)
+}
