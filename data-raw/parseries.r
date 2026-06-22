99a6dad93225edf2596f060f1598127b3c254537
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2026-06-22 15:18:04 -0300

    Upd series de teste internas do pacote

--- data-raw/parseries.r
+++ data-raw/parseries.r
@@ -1,13 +1,13 @@
-library(aws.s3)
 library(arrow)
+library(data.table)
+
+DIR <- "/home/lkhenayfis/Documents/ONS/MODELOS/NEWAVE/modelos-comuns/dados/data/vazoes"
 
 cods    <- c("1", "6", "8")
 objects <- paste0("vazoes-codigo=", cods, ".parquet.gzip")
-objects <- paste0("modelos/cenarios/modelos-comuns/dados/vazoes/", objects)
+objects <- file.path(DIR, objects)
 
-BUCKET <- "s3://ons-pem-historico"
-
-dummyseries <- lapply(objects, function(obj) s3read_using(read_parquet, object = obj, bucket = BUCKET))
+dummyseries <- lapply(objects, read_parquet)
 dummyseries <- lapply(dummyseries, function(d) ts(d$incremental, start = c(1931, 1), freq = 12))
 dummyseries <- do.call(cbind, dummyseries)
 colnames(dummyseries) <- c("serie1", "serie2", "serie3")
